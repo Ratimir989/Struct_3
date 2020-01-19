ba5466b96a76a1409b72168d22eb2adeb9335d35
Ratimir <ra0953029364@gmail.com>
2020-01-20 01:45:24 +0200

    Добавьте файлы проекта.

---
+++ Struct_3/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace Struct_3
+{
+    struct MyStruct
+    {
+        public string change;
+    }
+    class MyClass
+    {
+        public string change;
+    }
+    class Program
+    {
+        static void ClassTaker(MyClass myClass)
+        {
+            myClass.change = "изменен";
+        }
+        static void StructTaker(MyStruct myStruct)
+        {
+            myStruct.change = "изменена";
+        }
+        static void Main(string[] args)
+        {
+            MyClass myClass = new MyClass();
+            MyStruct myStruct = new MyStruct();
+            myClass.change = "не изменен";
+            myStruct.change = "не изменена";
+            ClassTaker(myClass);
+            StructTaker(myStruct);
+            Console.WriteLine("Структура: {0}\nКласс: {1}", myStruct.change, myClass.change);
+            Console.ReadKey();
+        }
+    }
+}
